055c8b644870f42d44b1feb2eb9ac116463005fa
victory-nwawunze <victorynwawunze@gmail.com>
2022-03-02 06:37:27 -0800

    updating carousel style

--- js/main.js
+++ js/main.js
@@ -14,5 +14,11 @@ $(document).ready(function () {
     loop: true,
     autoplay: true,
     autoplayTimeout: 3000,
+    dots: false,
+    nav: true,
+    navText: [
+      $(".owl-navigation .owl-nav-prev"),
+      $(".owl-navigation .owl-nav-next"),
+    ],
   });
 });

f4da814c6a492cbf607efbca543cd33aeab146aa
victory-nwawunze <victorynwawunze@gmail.com>
2022-03-02 07:20:22 -0800

    creating blog posts

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
   //   Owl-carousel for blog
   $(".owl-carousel").owlCarousel({
     loop: true,
-    autoplay: true,
+    autoplay: false,
     autoplayTimeout: 3000,
     dots: false,
     nav: true,

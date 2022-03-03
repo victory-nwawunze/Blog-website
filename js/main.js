f108cc80957d38ebfdde64ddbec1b01e5483d785
victory-nwawunze <victorynwawunze@gmail.com>
2022-03-03 06:47:46 -0800

    adding more responsive

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 /** @format */
 const responsive = {
+  0: {
+    items: 1,
+  },
   320: {
     items: 1,
   },
   560: {
     items: 2,
+  },
+  960: {
+    items: 3,
   },
 };
 $(document).ready(function () {
